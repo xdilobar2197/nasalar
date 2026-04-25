7b5406693a014187802828a9829ff104b515687d
Dilobar Xudayarova <xdilobar2197@gmail.com>
2026-04-25 13:36:38 +0500

    Add Curs class to main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# nasalar
+
+#     def area(self):
+#         return self.length * self.width
+#
+#     def perimeter(self):
+#         return 2 * (self.length + self.width)
+#
+#
+# r1 = Rectangle(5, 3)
+#
+# print("Yuza:", r1.area())
+# print("Perimetr:", r1.perimeter())
+
+
+class Curs:
+    def __init__(self, ism, kurs, yosh):
+        self.ism = ism
+        self.kurs = kurs
+        self.yosh = yosh
+
+oop = Curs('Mashxura', 1, 25)
